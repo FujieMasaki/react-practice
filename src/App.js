24ffb00a21d168009f6538d0a204f56301d21f28
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-03-24 22:57:08 +0900

    コメント追加

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
     <BrowserRouter>
     <div className="App">
       <Link to="/">Home</Link>
+      {/* 遷移するURL */}
       <br />
       <Link to="/page1">page1</Link>
       <br />
@@ -20,6 +21,7 @@ export default function App() {
     </div>
     <Routes>
     <Route path="/" element={<Home />} />
+    {/* 遷移するURLにもち付いた、コンポーネント */}
     <Route path="/page1" element={<Page1 />} />;
     <Route path="/page2" element={<Page2 />} />
     </Routes>
