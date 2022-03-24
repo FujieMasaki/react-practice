a912f3d2732b1e5ceafe681ac10e327939d7d79c
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-03-24 22:52:57 +0900

    Reactの基本のページ遷移

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { StyledComponents } from "./components/StyledComponents";
-import { StyledJsx} from "./components/StyledJsx.jsx";
-import { CssModules } from "./components/CssModules";
-import { InlineStyle } from "./components/InlineStyle";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+// Linkはhtmlのaタグのようなもの
+// SwitchはV.6から必要無くなった
+// RouteはSwitchの中でルートがマッチしたものを表示する
+import {Home} from "./Home";
+import {Page1} from "./Page1";
+import {Page2} from "./Page2";
 import "./styles.scss";
 
 export default function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/page1">page1</Link>
+      <br />
+      <Link to="/page2">page2</Link>
+      <br />
     </div>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/page1" element={<Page1 />} />;
+    <Route path="/page2" element={<Page2 />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
