22a11edd43ce279cf60891b1a7ae414ef9000f71
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-03-25 21:35:11 +0900

    共通するstyleをコンポーネント化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import { BrowserRouter, Link } from "react-router-dom";
-// Linkはhtmlのaタグのようなもの
-// SwitchはV.6から必要無くなった
-// RouteはSwitchの中でルートがマッチしたものを表示する
 import "./styles.scss";
-
-import { Router } from "./router/Router";
-
-
+import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
 //ネスト用のコンポーネントを作成
 export default function App() {
   return (
-    <BrowserRouter>
     <div className="App">
-      <Link to="/">Home</Link>
-      {/* 遷移するURL */}
-      <br />
-      <Link to="/page1">page1</Link>
-      <br />
-      <Link to="/page2">page2</Link>
-      <br />
+      <PrimaryButton>test</PrimaryButton>
+      <SecondaryButton>test</SecondaryButton>
     </div>
-    <Router/>
-    </BrowserRouter>
   );
 }
