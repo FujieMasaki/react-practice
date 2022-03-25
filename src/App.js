576548d9d01a71d63d957de4c586e643ca9755fa
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-03-25 22:58:12 +0900

    moleculesを作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles.scss";
 import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 //ネスト用のコンポーネントを作成
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>test</PrimaryButton>
       <SecondaryButton>test</SecondaryButton>
+      <br />
+      <SearchInput>検索</SearchInput>
     </div>
   );
 }
